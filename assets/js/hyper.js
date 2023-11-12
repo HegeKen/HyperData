6f92f29fb7bcf26bdeed1c2e898d2d2b7482e578
Hege <hegeken@foxmail.com>
2023-11-12 23:33:58 +0800

    BD

---
+++ assets/js/hyper.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c9e1b96ca1ad298ed068716ea5702f9c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
