c20710e5013e526c903d2573fb14eb8890e5ae1c
Hege <hegeken@foxmail.com>
2023-11-15 00:25:11 +0800

    2023.11.15 00:25:05

---
+++ template.vue
@@ -0,0 +1,9 @@
+<template>
+  <mdui-card style="width: 99vw;padding-bottom: 20px;align-items: center;margin-top:1vw;margin-right:1vw">
+    <div style="padding-left:10px;padding-top:10px">
+    </div>
+  </mdui-card>
+
+
+
+</template>
