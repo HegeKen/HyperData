d3dfeaccdea63f4a5df856f927eb77b56586a432
Hege <hegeken@foxmail.com>
2024-03-05 01:35:07 +0800

    2024.03.05 01:35:00

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -20,7 +20,8 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-currentStable = ["blue", "aurora", "houji", "nabu", "elish", "spesn", "spes", "viva", "veux", "zijin", "shennong_t", 
+currentStable = ["enuma",
+								 "blue", "aurora", "houji", "nabu", "elish", "spesn", "spes", "viva", "veux", "zijin", "shennong_t", 
 								 "evergo",  "sheng", "ziyi", "cetus" ,"lisa", "pissarro", "agate", "aristotle", "babylon",
 								 "corot","cupid", "dagu", "daumier","diting", "duchamp", "earth", "fire", "fuxi", "garnet", "gold",
 								 "ingres", "ishtar", "light", "lightcm","liuqin", "manet", "marble", "matisse", "mayfly",
@@ -60,7 +61,9 @@ flags = {
 	"RUBYTWGlobal": "ruby",
 	"VIVAGlobal":"viva",
 	"light_id_global": "light",
-	"light_id_global": "light",
+	"LIGHTIDGlobal": "light",
+	"enuma": "enuma",
+	"ENUMA": "enuma",
 	"zircon": "zircon",
 	"ZIRCON": "zircon",
 	"nabu": "nabu",
