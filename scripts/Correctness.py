5730ccc4f9c32f5c12c37ce93247438270291d1d
Hege <hegeken@foxmail.com>
2024-04-03 11:34:27 +0800

    2024.04.03 11:34:21

--- scripts/Correctness.py
+++ scripts/Correctness.py
@@ -5,11 +5,6 @@ def check_os_in_recovery_and_fastboot(data,device):
   for branch in data['branches']:
     roms = branch['roms']
     for os_version, rom_info in roms.items():
-      date_obj = datetime.strptime(rom_info['release'], "%Y-%m-%d")
-      if date_obj < datetime.now():
-        continue
-      else:
-        print(f"错误: 机型 "+{branch['name']['zh']}+"(" + device+ ") 所在的" + {os_version} +"处时间格式不对，请更正")
       if 'recovery' in rom_info and rom_info['recovery']:
         if os_version not in rom_info['recovery']:
           print(f"错误: OS {os_version} not found in recovery for branch {branch['name']['zh']} in device " + device)
