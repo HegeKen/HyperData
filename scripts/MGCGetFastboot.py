f39961f153319e34d3263329e68c489f68a94765
海格Hege <1804489753@qq.com>
2024-06-04 21:08:12 +0800

    2024.06.04 21:08:05

--- scripts/MGCGetFastboot.py
+++ scripts/MGCGetFastboot.py
@@ -2,7 +2,7 @@ import requests
 import json
 import OScommon
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76'}
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0'}
 headers = {'Connection': 'close'}
 domains = ['https://sgp-api.buy.mi.com/bbs/api/','https://ams-api.buy.mi.com/bbs/api/']
 params = '/phone/getlinepackagelist'
--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -1366,7 +1366,7 @@ def print_log(log):
 		
 
 def getFastboot(url):
-	headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76",
+	headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
 			   "Connection": "close"}
 	response = requests.post(url, headers=headers)
 	if (response.status_code == 200):
