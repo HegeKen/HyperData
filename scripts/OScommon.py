6fd5cbbebd3bc886cf1d479e3db0dd5b864595ac
海格Hege <1804489753@qq.com>
2024-07-10 13:59:01 +0800

    2024.07.10 13:58:56

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -21,7 +21,7 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-currentStable = ["agate", "air", "alioth", "ares", "aristotle", "aurora", "babylon", "breeze", "cas",
+currentStable = ["rothko", "agate", "air", "alioth", "ares", "aristotle", "aurora", "babylon", "breeze", "cas",
 								 "cetus", "chenfeng", "cmi", "corot", "cupid", "dagu", "daumier", "diting", "dizi",
 								 "duchamp", "earth", "elish", "emerald", "enuma", "evergo", "evergreen", "fire", "fleur", "fuxi",
 								 "gale", "garnet", "gold", "haydn", "houji", "ingres", "ishtar", "light", "lightcm",
