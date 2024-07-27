714100556457f4252636aefc1b03c10cc1c38fc5
海格Hege <1804489753@qq.com>
2024-07-27 20:33:01 +0800

    2024.07.27 20:32:55

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -21,7 +21,11 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-currentStable = ["earth", "elish", "emerald", "enuma", "evergo", "evergreen", "fire", "fleur", "fuxi",
+currentStable = ["ruyi", "goku", 
+								 "rothko", "agate", "air", "alioth", "ares", "aristotle", "aurora", "babylon", "breeze", "cas",
+								 "cetus", "chenfeng", "cmi", "corot", "cupid", "dagu", "daumier", "diting", "dizi",
+								 "duchamp", "earth", "elish", "emerald", "enuma", "evergo", "evergreen", "fire", "fleur", "fuxi",
+								 "earth", "elish", "emerald", "enuma", "evergo", "evergreen", "fire", "fleur", "fuxi",
 								 "gale", "garnet", "gold", "haydn", "houji", "ingres", "ishtar", "light", "lightcm",
 								 "lisa", "liuqin", "manet", "marble", "matisse", "mayfly", "mona", "mondrian", "moon", "moonstone",
 								 "munch", "nabu", "nuwa", "odin", "opal", "pearl", "peridot", "pipa", "pissarro", "pissarroin", "plato",
