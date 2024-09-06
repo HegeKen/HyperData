1821599af745e5388bcdd85903f43d81248edb58
Hege <hegeken@foxmail.com>
2024-09-06 20:55:17 +0800

    2024.09.06 20:55:08

--- scripts/Correctness.py
+++ scripts/Correctness.py
@@ -4,21 +4,29 @@ def check_os_in_recovery_and_fastboot(data,device):
   for branch in data['branches']:
     if data['device'] in branch['branchCode']:
       roms = branch['roms']
+      bname = branch['name']['zh']
       for os_version, rom_info in roms.items():
-        if 'recovery' in rom_info and rom_info['recovery']:
-          if os_version not in rom_info['recovery']:
-            print(f"错误: OS {os_version} not found in recovery for branch {branch['name']['zh']} in device " + device)
-          if rom_info['android'] not in rom_info['recovery']:
-            print(f"错误: OS {rom_info['android']} not found in recovery for branch {branch['name']['zh']} in device " + device)
-        if 'fastboot' in rom_info and rom_info['fastboot']:
-          if branch['branchCode'] != rom_info['fastboot'].split('_images')[0]:
-            print(f"错误: 机器线刷包包含的 {rom_info['fastboot'].split('_images')[0]} 与版本分支代码 {branch['branchCode']} 不一致")
-          if os_version not in rom_info['fastboot']:
-            print(f"错误: OS {os_version} not found in fastboot for branch {branch['name']['zh']} in device " + device)
-          if rom_info['android'] not in rom_info['fastboot']:
-            print(f"错误: OS {rom_info['android']} not found in fastboot for branch {branch['name']['zh']} in device " + device)
-    else:
-      print(f"错误: 机器 {data['device']} 与版本分支代码 {branch['branchCode']} 不一致")
+        if rom_info['android'] == "" and rom_info['os'] == "" and rom_info['release'] == "" and rom_info['recovery'] == "" and rom_info['fastboot'] == "":
+          i = 0
+        else:
+          if rom_info['android'] == "":
+            print(f"错误:机型 {device} {bname} {os_version} 安卓版本未标注")
+          if rom_info['os'] == "":
+            print(f"错误:机型 {device} {bname} {os_version} OS版本未标注")
+          if rom_info['release'] == "":
+            print(f"错误:机型 {device} {bname} {os_version} 发布时间未标注")
+          if rom_info['recovery'] != "":
+            if rom_info['android'] not in rom_info['recovery']:
+              print(f"错误:机型 {device} {bname} {os_version} 卡刷包与当前记录的安卓版本不一致")
+            if rom_info['os'] not in rom_info['recovery']:
+              print(f"错误:机型 {device} {bname} {os_version} 卡刷包与当前记录的OS版本不一致")
+          if rom_info['fastboot'] != "":
+            if rom_info['android'] not in rom_info['fastboot']:
+              print(f"错误:机型 {device} {bname} {os_version} 线刷包与当前记录的安卓版本不一致")
+            if rom_info['os'] not in rom_info['fastboot']:
+              print(f"错误:机型 {device} {bname} {os_version} 线刷包与当前记录的OS版本不一致")
+            if branch['branchCode'] not in rom_info['fastboot']:
+              print(f"错误:机型 {device} {bname} {os_version} 线刷包与当前记录的版本分支代码不一致")
 
 if __name__ == "__main__":
   for device in OScommon.currentStable:
