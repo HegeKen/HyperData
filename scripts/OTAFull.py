9f58c4dd89baa4fda882a123893fd85d4fcafecf
Hege <hegeken@foxmail.com>
2024-09-09 12:34:16 +0800

    2024.09.09 12:34:08

--- scripts/OTAFull.py
+++ scripts/OTAFull.py
@@ -12,18 +12,22 @@ for device in OScommon.currentStable:
     current = devdata['branches'][num]["roms"][list(devdata['branches'][num]["roms"].keys())[0]]
     android = current['android']
     version = current['os']
-    if branch == 'X' or branch == 'D' or "Enterprise" in devdata["branches"][num]["name"]["en"] or "EP" in devdata["branches"][num]["name"]["en"]:
-      print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为"+version,end="               ", flush=True)
-      OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
+    release = current['release']
+    if android == "" and version == "" and release == "":
+      i = 0
     else:
-      for rom in devdata['branches'][num]["roms"]:
-        for i in range(-3,3):
-          if int(devdata['branches'][num]["roms"][rom]['os'].split('.')[2])+i > 0:
-            version = OScommon.versionAdd(devdata['branches'][num]["roms"][rom]['os'],i)
-            if version in devdata:
-              i = 0
+      if branch == 'X' or branch == 'D' or "Enterprise" in devdata["branches"][num]["name"]["en"] or "EP" in devdata["branches"][num]["name"]["en"]:
+        print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为"+version,end="               ", flush=True)
+        OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
+      else:
+        for rom in devdata['branches'][num]["roms"]:
+          for i in range(-3,3):
+            if int(devdata['branches'][num]["roms"][rom]['os'].split('.')[2])+i > 0:
+              version = OScommon.versionAdd(devdata['branches'][num]["roms"][rom]['os'],i)
+              if version in devdata:
+                i = 0
+              else:
+                print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为"+version,end="               ", flush=True)
+                OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
             else:
-              print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为"+version,end="               ", flush=True)
-              OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
-          else:
-            i = 0
+              i = 0
