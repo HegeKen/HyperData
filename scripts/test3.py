81dad4e06f70d5ea7af4a9c0d69ebe82a9869b02
HegeKen <hegeken@foxmail.com>
2025-03-02 23:58:12 +0800

    2025-03-02-23:58:12

--- scripts/test3.py
+++ scripts/test3.py
@@ -1,6 +1,8 @@
 import OScommon
 
-roms = ["miui_VENUS_OS1.0.12.0.UKBCNXM_77aa77ab20_14.0.zip"]
-
-for rom in roms:
-  OScommon.checkDatabase(rom)
+file = open("public/data/scripts/NewROMs.txt", "r", encoding='utf-8')
+lines = file.readlines()
+for line in lines:
+  line = line.rstrip()
+  device, code, android, version, type, bigver, region,tag,zone, branch, filetype, filename = [item for item in OScommon.getData(line)]
+  OScommon.checkDatabase(device, code, android, version, type, bigver, region,tag,zone, branch, filetype, filename)
