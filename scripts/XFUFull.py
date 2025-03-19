2ba881bd19f04f71ab3387ead3d75cce60dcd13f
Hege <hegeken@foxmail.com>
2025-03-19 12:51:01 +0800

    2025-03-19-12:51:01

--- scripts/XFUFull.py
+++ scripts/XFUFull.py
@@ -6,7 +6,8 @@ import OScommon
 false_packs = [
   'OS1.0.4.0.UNKCNXMmiui_VERMEER_OS1.0.4.0.UNKCNXM_c3235c755f_14.0.zip'
 ]
-directory = "D:\\Projects\\MIUIROMS\\XFUOrigin\\pages\\hyperos"
+# directory = "D:\\Projects\\MIUIROMS\\XFUOrigin\\pages\\hyperos"
+directory = "../Sources/xmfirmwareupdater.github.io/pages/hyperos"
 
 for root, dirs, files in os.walk(directory):
   for file in files:
@@ -14,7 +15,7 @@ for root, dirs, files in os.walk(directory):
     with open(file_path, 'r', encoding='utf-8') as f:
       content = f.read()
       soup = BeautifulSoup(content, 'lxml')
-      span_tags = soup.findAll('span', {'id': 'filename'})
+      span_tags = soup.find_all('span', {'id': 'filename'})
       for tag in span_tags:
         if tag.text in false_packs:
           continue
