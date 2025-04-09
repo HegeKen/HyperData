cd84bd843455e109a0cf21b50b456a731125f985
Hege <hegeken@foxmail.com>
2025-04-10 00:43:01 +0800

    2025-04-10-00:43:01

--- scripts/XFUFull.py
+++ scripts/XFUFull.py
@@ -1,19 +1,21 @@
 import os
+import chardet
 from bs4 import BeautifulSoup
 import OScommon
-
 
 false_packs = [
   'OS1.0.4.0.UNKCNXMmiui_VERMEER_OS1.0.4.0.UNKCNXM_c3235c755f_14.0.zip'
 ]
-# directory = "D:\\Projects\\MIUIROMS\\XFUOrigin\\pages\\hyperos"
 directory = "../Sources/xmfirmwareupdater.github.io/pages/hyperos"
 
 for root, dirs, files in os.walk(directory):
   for file in files:
     file_path = os.path.join(root, file)
-    with open(file_path, 'r', encoding='utf-8') as f:
-      content = f.read()
+    with open(file_path, 'rb') as f:
+      raw_content = f.read()
+      result = chardet.detect(raw_content)
+      encoding = result['encoding']
+      content = raw_content.decode(encoding, errors='replace')
       soup = BeautifulSoup(content, 'lxml')
       span_tags = soup.find_all('span', {'id': 'filename'})
       for tag in span_tags:
