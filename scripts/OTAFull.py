2d9a610587050a01c2609d98158f6bbd1c47b96e
Hege <hegeken@foxmail.com>
2025-05-12 11:37:12 +0800

    2025-05-12-11:37:12

--- scripts/OTAFull.py
+++ scripts/OTAFull.py
@@ -25,12 +25,15 @@ for device in OScommon.currentStable:
         else:
           for rom in devdata['branches'][num]["roms"]:
             for i in range(-3,3):
-              if int(devdata['branches'][num]["roms"][rom]['os'].split('.')[2])+i > 0:
-                version = OScommon.versionAdd(devdata['branches'][num]["roms"][rom]['os'],i)
-                if version in devdata:
-                  i = 0
-                else:
-                  print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为: "+version,end="               ", flush=True)
-                  OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
-              else:
+              if version == "":
                 i = 0
+              else:
+                if int(devdata['branches'][num]["roms"][rom]['os'].split('.')[2])+i > 0:
+                  version = OScommon.versionAdd(devdata['branches'][num]["roms"][rom]['os'],i)
+                  if version in devdata:
+                    i = 0
+                  else:
+                    print("\r",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"\t正在完成 "+device+"， 分支为："+devdata["branches"][num]["branchCode"]+"，版本为: "+version,end="               ", flush=True)
+                    OScommon.getFromApi(OScommon.miui_encrypt(OScommon.OTAFormer(device, code, region, branch, zone, android, version)))
+                else:
+                  i = 0
