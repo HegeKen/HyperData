af9f288e43669bcced59026aca9a61ea573e348c
Hege <hegeken@foxmail.com>
2025-06-16 13:24:59 +0800

    2025-06-16-13:24:59

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -106,8 +106,8 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-unreleased = ["dali", "turner", "violin"]
-currentStable = ["dali", "turner", "violin", "koto", "taiko", "dijun", "jinghu", "luming", "onyx", "poussin", "citrine", "serenity", "emerald_r", "miro", "zorn", "xuanyuan", "tanzanite", "obsidian", "rodin", "warm", "dada", "haotian", "uke", "muyu", 
+unreleased = ["dali", "turner", "violin", "goya", "klimt", "bixi"]
+currentStable = ["goya", "klimt", "bixi", "dali", "turner", "violin", "koto", "taiko", "dijun", "jinghu", "luming", "onyx", "poussin", "citrine", "serenity", "emerald_r", "miro", "zorn", "xuanyuan", "tanzanite", "obsidian", "rodin", "warm", "dada", "haotian", "uke", "muyu", 
 								 "beryl", "amethyst", "malachite", "degas", "rothko", "flame", "lake", "flare", "spark", 
 								 "ruyi", "goku", "agate", "air", "alioth", "ares", "aristotle", "aurora", "babylon", "breeze", "cas",
 								 "cetus", "chenfeng", "cmi", "corot", "cupid", "dagu", "daumier", "diting", "dizi",
@@ -135,12 +135,12 @@ order = ['umi', 'cmi', 'cas', 'thyme', 'venus', 'star', 'lisa', 'pissarro_in', '
 				 'houji', 'shennong', 'shennong_t', 'aurora', 'degas', "dada", "haotian","xuanyuan", "dijun", 'nabu', 'enuma', 'elish', 'dagu', 'pipa',
 				 'liuqin', 'yudi','sheng', "uke", "muyu", "jinghu", "violin", 'odin', 'cetus', 'zizhan', 'babylon', 'goku', 'ruyi', 'mona',
 				 'zijin', 'ziyi', 'yuechu', 'chenfeng', "luming",
-				 'fire', 'earth', 'sky', 'gale', 'lake', 'flame', 'evergo', 'light', 'lightcm', 'veux', "xaga", 'pissarro',
+				 'fire', 'earth', 'sky', 'gale', 'moon', 'air', 'lake', 'flame', 'evergo', 'light', 'lightcm', 'veux', "xaga", 'pissarro',
 				 'spes', 'spesn', 'viva', 'vida', 'fleur', 'opal', 'sunstone', 'ruby', 'redwood', 'pearl', 'marble', 'tapas', 'topaz',
 				 'sweet_k6a', 'sea', 'gold', 'breeze', 'garnet', 'emerald', 'zircon', 'tanzanite', 'obsidian', 'beryl', 'malachite', 'amethyst', 'sapphire', 'sapphiren', "emerald_r", 'peridot', 'rodin',"onyx", 'alioth',
 				 'haydn', 'ares', 'munch', 'rubens', 'matisse', 'ingres', 'diting', 'rembrandt', 'mondrian', 'socrates', 'corot', 'duchamp',
 				 'vermeer', 'manet', 'rothko', "zorn", "miro",
-				 'yunluo', 'xun', 'flare', 'spark', "koto", "taiko", 'dizi', 'ruan', 'warm', "serenity", 'evergreen', 'rock', 'moon', 'air', 'moonstone']
+				 'yunluo', 'xun', 'flare', 'spark', "koto", "taiko", 'dizi', 'ruan', 'warm', "serenity", 'evergreen', 'rock', 'moonstone']
 
 branches = [
 	{
