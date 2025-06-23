cac04be6859a6d796e3e572388e7c287ebe30850
Hege <hegeken@foxmail.com>
2025-06-23 15:02:21 +0800

    2025-06-23-15:02:21

--- scripts/FlagComplete.py
+++ scripts/FlagComplete.py
@@ -1,8 +1,8 @@
 import OScommon
 import sys
-sys.path.append("D:\\Projects\\HyperOS.fans\\Nuxt3MR\\public\\MRData\\scripts\\")
-import common
-
+# sys.path.append("D:\\Projects\\HyperOS.fans\\Nuxt3MR\\public\\MRData\\scripts\\")
+sys.path.append("../NuxtMR/public/MRData/scripts/")
+import common # type: ignore
 
 for flag in OScommon.flags:
   if flag in common.flags:
