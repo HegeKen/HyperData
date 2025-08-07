997cc8a8d0d7f2086cb8efb54b5cb5fb4d9b2ad0
Hege <hegeken@foxmail.com>
2025-08-07 18:26:09 +0800

    2025-08-07-18:26:09

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -106,8 +106,8 @@ miui_iv = b"0102030405060708"
 check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
-unreleased = ["goya", "klimt"]
-currentStable = ["taiko", "bixi", "dali", "turner", "goya", "klimt", "violin", "koto", "dijun", "jinghu", "luming", "onyx", "poussin", "citrine", "serenity", "emerald_r", "miro", "zorn", "xuanyuan", "tanzanite", "obsidian", "rodin", "warm", "dada", "haotian", "uke", "muyu", 
+unreleased = ["spring", "goya", "klimt"]
+currentStable = ["spring", "taiko", "bixi", "dali", "turner", "goya", "klimt", "violin", "koto", "dijun", "jinghu", "luming", "onyx", "poussin", "citrine", "serenity", "emerald_r", "miro", "zorn", "xuanyuan", "tanzanite", "obsidian", "rodin", "warm", "dada", "haotian", "uke", "muyu", 
 								 "beryl", "amethyst", "malachite", "degas", "rothko", "flame", "lake", "flare", "spark", 
 								 "ruyi", "goku", "agate", "air", "alioth", "ares", "aristotle", "aurora", "babylon", "breeze", "cas",
 								 "cetus", "chenfeng", "cmi", "corot", "cupid", "dagu", "daumier", "diting", "dizi",
