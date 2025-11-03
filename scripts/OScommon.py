abb73a2c4d747dab0a46185488d3066e649bfdea
Hege <hegeken@foxmail.com>
2025-11-03 21:33:25 +0800

    2025-11-03-21:33:25

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -1917,6 +1917,7 @@ flags = {
 	"ruby_id_global": "ruby",
 	"LIUQINDEMO": "liuqin",
 	"spring_lm_cr_global": "spring",
+	"organ_ru_global": "organ",
 	"YUDIDEMO": "yudi",
 	"pipa_demo": "pipa",
 	"yuechu_demo": "yuechu",
