78e4b50c172e86f09381615282c29546c0770a06
Hege <hegeken@foxmail.com>
2026-09-07 20:47:49 +0800

    2026-09-07-20:47:49

--- scripts/OScommon.py
+++ scripts/OScommon.py
@@ -174,7 +174,7 @@ check_url = "https://update.miui.com/updates/miotaV3.php"
 
 
 unreleased = ['lhasa', 'moscow', 'brussels', 'chicago', 'pond', 'suiren', 'zephyr', 'coral']
-currentStable = ['brussels', 'chicago','lhasa', 'athens', 'songyuan', 'steppe', 'leedsa', 'mist', 'iolite', 'moscow',  'pond',
+currentStable = ['lhasa', 'brussels', 'chicago', 'athens', 'songyuan', 'steppe', 'leedsa', 'mist', 'iolite', 'moscow',  'pond',
 								 'warsaw', 'chagall','warhol', 'erhu', 'byron', 'guitar', 'yili', 'prague', 'somalia', 'zephyr', 'suiren', 'coral', 'arctic', 'dew',
 								 'klee', 'dash', 'piano', 'yupei', 'pudding', 'nezha', 'flute', 'organ', 'spinel','charoite','annibale', 'myron',
 								 'pandora', 'popsicle', 'tornado','goya', 'klimt', 'konghou',  'spring', 'lapis', 'kunzite',
@@ -839,6 +839,8 @@ flags = {
 	"muyu_tw_global":"muyu",
 	"muyu_eea_global":"muyu",
 	"muyu_global":"muyu",
+	"lhasa": "lhasa",
+	"lhasa_demo": "lhasa",
 	"uke_id_global":"uke",
 	"uke_ru_global":"uke",
 	"uke_tw_global":"uke",
